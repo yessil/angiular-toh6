8e1ade9453d19171be67966a78f21b4e9e2791b0
Yessil Khassenov <yessil@sapa-astana.kz>
2022-03-31 12:37:39 +0600

    1st

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HeroesComponent } from './heroes/heroes.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
